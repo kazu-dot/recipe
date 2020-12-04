24d9b9ae598aa5a7fdbe2fee25e0adfe95a35b1c
kazu-dot <takahashikazuhiro90@icoud.com>
2020-12-04 14:44:01 +0900

    Feat/ログイン、ログアウ機能のテスト”

--- spec/requests/public_request_spec.rb
+++ spec/requests/public_request_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe "Publics", type: :request do
 
   describe "GET /home" do
-    it "returns http success" do
-      get "/public/home"
-      expect(response).to have_http_status(:success)
-    end
   end
 
 end
