b5688a6ac7f1f5e21bf188142e2b3854fa90103b
kazu-dot <takahashikazuhiro90@icoud.com>
2020-12-13 15:59:54 +0900

    Fix:devise.ja.yamlの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe User, type: :model do
   it "名前がなければ無効な状態であること" do
     user = FactoryBot.build(:user, username: nil)
     user.valid?
-    expect(user.errors[:username]).to include("can't be blank")
+    expect(user.errors[:username]).to include("を入力してください")
   end
 
   it "名前が長過ぎれば無効な状態であること"
@@ -32,14 +32,14 @@ RSpec.describe User, type: :model do
   it "メールアドレスがなければ無効な状態であること" do
     user = FactoryBot.build(:user, email: nil)
     user.valid?
-    expect(user.errors[:username]).to include("can't be blank")
+    expect(user.errors[:email]).to include("を入力してください")
   end
 
 
   it "重複したメールアドレスなら無効な状態であること" do
     FactoryBot.create(:user, email: "dupulicated@example.com")
 
-    user = FactoryBot.build(:user, email: "upulicated@example.com")
+    user = FactoryBot.build(:user, email: "dupulicated@example.com")
 
     expect(user).to_not be_valid
   end
