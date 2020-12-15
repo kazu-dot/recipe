8a87dfaa1cda08d38add6801df91ca46788adfbd
kazu-dot <takahashikazuhiro90@icoud.com>
2020-12-15 16:11:00 +0900

    refactor:ja.ymlの修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
+  validates :title  , presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 140 }
 end
--- db/migrate/20201204060220_create_microposts.rb
+++ db/migrate/20201204060220_create_microposts.rb
@@ -1,6 +1,7 @@
 class CreateMicroposts < ActiveRecord::Migration[6.0]
   def change
     create_table :microposts do |t|
+      t.text :title
       t.text :content
       t.references :user, null: false, foreign_key: true
 
