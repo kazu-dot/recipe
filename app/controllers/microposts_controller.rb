d3f249f6300f0c1d8484038888499a42765387dc
kazu-dot <takahashikazuhiro90@icoud.com>
2020-12-18 14:55:44 +0900

    投稿フォームの作成

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,21 @@
 class MicropostsController < ApplicationController
+  before_action :set_user
+
+  def new
+    @micropost = Micropost.new
+  end
+
+  def show
+
+  end
+
+  private
+
+  def micropost_parameter
+    params.require(:micropost).permit(:title, :content).merge(user_id: current_user.id)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
