6b5bfa4fcdaca66bfdfe4072ddd9af6d242068fa
kazu-dot <takahashikazuhiro90@icoud.com>
2020-12-23 13:41:32 +0900

    _micropost.html.erbの編集

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Recipe
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.beginning_of_week = :sunday
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
