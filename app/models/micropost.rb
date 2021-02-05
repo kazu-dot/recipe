d43574ed7a511d139b8734f8a7381252c899edd1
kazu-dot <takahashikazuhiro90@icoud.com>
2021-02-05 14:39:05 +0900

    タグ、中間テーブルモデルの設定

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,9 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :tag_maps, dependent: :destroy
+  has_many :tags, through: :tag_maps
+
   validates :user_id, presence: true
   validates :title  , presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 140 }
