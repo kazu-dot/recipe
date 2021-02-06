b4633676b2d970edf2ce2cdddba072d214925d50
kazu-dot <takahashikazuhiro90@icoud.com>
2021-02-07 00:28:57 +0900

    tag_map, tag モデルのテスト

--- spec/models/tag_map_spec.rb
+++ spec/models/tag_map_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TagMap, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tag_map){FactoryBot.create(:tag_map)}
+
+  it "has a valid factory" do
+    expect(tag_map).to be_valid
+  end
+
+  it "micropost_idがnilであれば無効な状態であること" do
+    tag_map.micropost_id = nil
+    expect(tag_map.valid?).to eq false
+  end
+
+  it "tag_idがnilであれば無効な状態であること" do
+    tag_map.tag_id = nil
+    expect(tag_map.valid?).to eq false
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tag){FactoryBot.create(:tag)}
+
+  it "has a valid factory" do
+    expect(tag).to be_valid
+  end
+
+  it "タグ名が空欄であれば無効な状態であること" do
+    tag.tag_name = nil
+    expect(tag.valid?).to eq false
+  end
+
+  it "タグ名が多ければ無効な状態であること" do
+    tag.tag_name = 'a' * 21
+    expect(tag.valid?).to eq false
+  end
 end
