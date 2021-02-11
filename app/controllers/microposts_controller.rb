4e93297cf02748a2d8bdcb576ff229248e7f802b
kazu-dot <takahashikazuhiro90@icoud.com>
2021-02-11 13:05:05 +0900

    編集ページの作成

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,16 +1,19 @@
 class MicropostsController < ApplicationController
-  before_action :set_user
   before_action :correct_user,   only: :destroy
 
   def new
     @micropost = Micropost.new
-
   end
 
   def show
     @micropost = Micropost.find(params[:id])
-    @comments = @micropost.comments
-    @comment = Comment.new
+    if (@micropost.published? || user_signed_in? && @micropost.draft?)
+      @comments = @micropost.comments
+      @comment = Comment.new
+    else
+      flash[:alert] = "非公開です ログインしてください"
+      redirect_to root_path
+    end
   end
 
   def index
@@ -28,6 +31,20 @@ class MicropostsController < ApplicationController
     else
       flash[:alert] = '投稿に失敗しました'
       render :new
+    end
+  end
+
+  def edit
+    @micropost = Micropost.find(params[:id])
+  end
+
+  def update
+    @micropost = Micropost.find(params[:id])
+    if @micropost.update(micropost_params)
+      flash[:create] = 'UPDATE !'
+      redirect_to micropost_path(@micropost)
+    else
+      render :edit
     end
   end
 
@@ -53,11 +70,7 @@ class MicropostsController < ApplicationController
   private
 
   def micropost_params
-    params.require(:micropost).permit(:title, :content).merge(user_id: current_user.id)
-  end
-
-  def set_user
-    @user = User.find(current_user.id)
+    params.require(:micropost).permit(:title, :content, :status).merge(user_id: current_user.id)
   end
 
   def correct_user
