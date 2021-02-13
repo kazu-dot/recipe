81a51a6e48ec4daf6d070536ed6f3f266ec21f10
kazu-dot <takahashikazuhiro90@icoud.com>
2021-02-13 15:11:12 +0900

    UserModel,Controllerの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  mount_uploader :image, ImageUploader
+
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_060934) do
+ActiveRecord::Schema.define(version: 2021_02_13_060545) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_02_05_060934) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.integer "status", default: 0, null: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
@@ -104,6 +105,7 @@ ActiveRecord::Schema.define(version: 2021_02_05_060934) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
